b50b4af6a45257e75f69696c6411605e7ac911cd
diegorezm <dmourarezende@gmail.com>
2020-09-02 10:45:42 -0300

    readme update

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 #       St
 My build of suckless [st](https://st.suckless.org/)
 
-#       Patches
+##       Patches
 - St clipboard
 - font 2
 - Xresources
 - scrollback
+
+##      Installation guide
+```
+git clone https://github.com/diegorezm/st-d.git
+cd st-d
+sudo make install
+```
--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMono NL:pixelsize=16:antialias=true:autohint=true";
+static char *font = "JetBrainsMono NL:pixelsize=14:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
 	"Inconsolata for Powerline:pixelsize=14:antialias=true:autohint=true", 
